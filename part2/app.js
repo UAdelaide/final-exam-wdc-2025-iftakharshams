db7069e5477182c65b083e64f9bed22307ef7ac6
Iftakhar Shams <55760905+iftakharshams@users.noreply.github.com>
2025-06-21 05:52:43 +0000

    [automatic commit] 2025-06-21 05:52:43 UTC 129.127.202.31

--- part2/app.js
+++ part2/app.js
@@ -18,5 +18,23 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+// GET all dog data
+app.get('/api/dogs', async function(req, res) {
+  try {
+    const [rows] = await db.query(
+      `SELECT
+        dog_id,
+        name AS dog_name,
+        size,
+        owner_id
+      FROM Dogs`
+    );
+    return res.status(200).json(rows);
+  } catch(err) {
+    console.error("Error getting dogs data");
+    return res.sendStatus(500);
+  }
+});
+
 // Export the app instead of listening here
 module.exports = app;
