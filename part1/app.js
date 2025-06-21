730bf4e6b55bc1b038bdef8c939861f75d5192a1
Iftakhar Shams <55760905+iftakharshams@users.noreply.github.com>
2025-06-21 02:11:37 +0000

    [automatic commit] 2025-06-21 02:11:37 UTC 129.127.202.31

--- part1/app.js
+++ part1/app.js
@@ -23,9 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ------------------------------------------------------------
 // Minimal Express + MySQL demo for the “dog-walking” exam part
 // ------------------------------------------------------------
-const express = require('express');
-const mysql   = require('mysql2/promise');
-const app     = express();
 const PORT    = process.env.PORT || 8080;
 
 /* ---------- 1.  Database pool ---------- */
