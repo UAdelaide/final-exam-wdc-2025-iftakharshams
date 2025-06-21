be04f6e3cf39597d0ab1fdaa37ecb46d2038c711
Iftakhar Shams <55760905+iftakharshams@users.noreply.github.com>
2025-06-21 04:21:35 +0000

    [automatic commit] 2025-06-21 04:21:35 UTC 129.127.202.31

--- part1/app.js
+++ part1/app.js
@@ -1,90 +1,123 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-var db;
+let db;
 
 (async () => {
   try {
     // Connect to MySQL without specifying a database
     const connection = await mysql.createConnection({
+      // host: 'localhost',
       socketPath: '/var/run/mysqld/mysqld.sock',
-      host: '127.0.0.1',
       user: 'root',
-      password: '', // Set MySQL root password
-      database: 'DogWalkService'
+      password: 'root' // Set your MySQL root password
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
     await connection.end();
 
     // Now connect to the created database
     db = await mysql.createConnection({
+      // host: 'localhost',
       socketPath: '/var/run/mysqld/mysqld.sock',
-      host: '127.0.0.1',
       user: 'root',
-      password: '',
+      password: 'root',
       database: 'DogWalkService'
     });
 
-    // Create a table if it doesn't exist
+    // Add record to show up back-end implemetation
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS Dogs (
-        dog_id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(50) NOT NULL,
-        size ENUM('small', 'medium', 'large') NOT NULL
-      )
+      INSERT INTO Users (username, email, password_hash, role)
+      VALUES
+      ('alice123', 'alice@example.com', 'hashed123','owner'),
+      ('bobwalker', 'bob@example.com', 'hashed456','walker'),
+      ('carol123', 'carol@example.com', 'hashed789','owner')
     `);
 
-    // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
-    if (rows[0].count === 0) {
-      await db.execute(`
-        INSERT INTO Dogs (name, size) VALUES
-        (  'Max',  'medium'),
-        (  'Bella',  'small'),
-        (  'Rocky',  'large'),
-        (  'Charlie',  'medium'),
-        (  'Luna',  'large')
-      `);
-    }
+    await db.execute(`
+      INSERT INTO Dogs (owner_id, name, size)
+      VALUES
+      ((SElECT user_id FROM Users WHERE email = 'alice@example.com' LIMIT 1),'Max', 'medium'),
+      ((SElECT user_id FROM Users WHERE email = 'carol@example.com' LIMIT 1), 'Bella', 'small'),
+      ((SElECT user_id FROM Users WHERE email = 'alice@example.com' LIMIT 1), 'Den', 'large'),
+      ((SElECT user_id FROM Users WHERE email = 'carol@example.com' LIMIT 1), 'PhuLoc', 'medium'),
+      ((SElECT user_id FROM Users WHERE email = 'alice@example.com' LIMIT 1), 'LuongPhuoc', 'small')
+    `);
+
+    await db.execute(`
+      INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+      VALUES
+      ((SELECT dog_id FROM Dogs WHERE name = 'Max' LIMIT 1), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+      ((SELECT dog_id FROM Dogs WHERE name = 'Bella' LIMIT 1), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+      ((SELECT dog_id FROM Dogs WHERE name = 'Bella' LIMIT 1), '2025-07-12 08:00:00', 45, 'Moon', 'completed'),
+      ((SELECT dog_id FROM Dogs WHERE name = 'PhuLoc' LIMIT 1), '2025-04-14 08:00:00', 100, 'Renown Park', 'completed'),
+      ((SELECT dog_id FROM Dogs WHERE name = 'LuongPhuoc' LIMIT 1), '2025-08-20 08:00:00', 20, 'Fuji', 'open')
+    `);
+
+    await db.execute(`
+      INSERT INTO Users (username, email, password_hash, role)
+      VALUES
+        ('noratingwalker', 'no@ratings.com', 'pw1', 'walker'),
+        ('nowalkswalker', 'no@walks.com', 'pw2', 'walker')
+    `);
+
+    await db.execute(`
+      INSERT INTO WalkApplications (request_id, walker_id, status)
+      VALUES
+        (3, 2, 'accepted'),
+        (4, 2, 'accepted'),
+        (1, 5, 'pending')
+    `);
+
+    await db.execute(`
+      INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating)
+      VALUES
+        (3, 2, 1, 5),
+        (4, 2, 3, 4)
+    `);
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
 })();
 
-// Route to return dogs as JSON
-app.get('/', async (req, res) => {
-  try {
-    const [dogs] = await db.execute('SELECT * FROM Dogs');
-    res.json(dogs);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
-  }
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
-// routers
-var dogsRouter = require('./routes/dogs');
-var walkersRouter = require('./routes/walkers');
-var walkRequestsRouter = require('./routes/walkrequests');
-
-app.use('/api/dogs', dogsRouter);
-app.use('/api/walkers/summary', walkersRouter);
-app.use('/api/walkrequests/open', walkRequestsRouter);
-
-
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
