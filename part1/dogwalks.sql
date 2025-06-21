8afea6f269d690f3915b9b3ba110815a06b84615
Iftakhar Shams <55760905+iftakharshams@users.noreply.github.com>
2025-06-21 01:27:00 +0000

    [automatic commit] 2025-06-21 01:27:00 UTC 129.127.202.31

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -77,24 +77,24 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
   ((SELECT dog_id FROM Dogs
       WHERE name = 'Max'
         AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),
-     '2025-06-10 08:00:00', 30, 'Parklands',      'open'),
+     '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 
   ((SELECT dog_id FROM Dogs
       WHERE name = 'Bella'
         AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),
-     '2025-06-10 09:30:00', 45, 'Beachside Ave',  'accepted'),
+     '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 
   ((SELECT dog_id FROM Dogs
       WHERE name = 'Rocky'
         AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),
-     '2025-06-11 10:00:00', 60, 'Riverbank Rd',   'open'),
+     '2025-06-11 10:00:00', 60, 'Riverbank Rd', 'open'),
 
   ((SELECT dog_id FROM Dogs
       WHERE name = 'Charlie'
         AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),
-     '2025-06-12 07:30:00', 30, 'Central Park',   'open'),
+     '2025-06-12 07:30:00', 30, 'Central Park', 'open'),
 
   ((SELECT dog_id FROM Dogs
       WHERE name = 'Luna'
         AND owner_id = (SELECT user_id FROM Users WHERE username = 'emilyowner')),
-     '2025-06-12 18:00:00', 45, 'Sunset Trail',   'open');
+     '2025-06-12 18:00:00', 45, 'Sunset Trail', 'open');
