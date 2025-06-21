6cf9427a4541e008d420fc86e044d0492550d76a
Iftakhar Shams <55760905+iftakharshams@users.noreply.github.com>
2025-06-21 06:16:48 +0000

    [automatic commit] 2025-06-21 06:16:47 UTC 129.127.202.31

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -28,6 +28,21 @@ router.post('/register', async (req, res) => {
   }
 });
 
+//q15
+router.get('/dogs', async(req,res) => {
+  const {owner_id} = req.query;
+
+  try{
+    const[dogs] = await db.query (
+      `SELECT dog_id, name FROM Dogs WHERE owner_id = ?`,
+      [owner_id]
+    );
+    res.json(dogs);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
 router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
@@ -35,24 +50,42 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
+
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  //changed to username instead of email
+  const { username, password } = req.body;
 
   try {
+    //changed to username instead of email
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
-    `, [email, password]);
+      WHERE username = ? AND password_hash = ?
+    `, [username, password]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    //for session implementation
+    req.session.user = rows[0];
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
+});
+
+//adding logout route
+router.post('/logout', (req, res) => {
+  //destroys session form login
+  req.session.destroy(err => {
+    if(err) {
+      return res.status(500).json({ error: 'failed to logout'});
+    }
+    res.clearCookie('dogwalk.sid');
+    res.status(200).json({ message: 'logout success!'});
+  });
 });
 
 module.exports = router;
