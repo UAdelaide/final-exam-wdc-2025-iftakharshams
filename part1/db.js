94ac0bbdacd6df9afbb541d978e2ffca2a3d3ff4
Iftakhar Shams <55760905+iftakharshams@users.noreply.github.com>
2025-06-21 03:58:10 +0000

    [automatic commit] 2025-06-21 03:58:10 UTC 129.127.202.31

--- part1/db.js
+++ part1/db.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  database: 'DogWalkService'
+    socketPath: '/var/run/mysqld/mysqld.sock',
+    host: '127.0.0.1',
+    user: 'root',
+    password: '', // Set MySQL root password
+    database: 'DogWalkService'
 });
 
-module.exports = pool;
+module.exports = pool.promise();
