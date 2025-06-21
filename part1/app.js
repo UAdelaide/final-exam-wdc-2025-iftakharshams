435e7eeb4b2d6f084499f78df13452a4c0ad18d4
Iftakhar Shams <55760905+iftakharshams@users.noreply.github.com>
2025-06-21 01:45:24 +0000

    [automatic commit] 2025-06-21 01:45:23 UTC 129.127.202.31

--- part1/app.js
+++ part1/app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index'); */
-var usersRouter = require('./routes/users'); */
+/* var indexRouter = require('./routes/index'); */
+/* var usersRouter = require('./routes/users'); */
 
 var app = express();
 
@@ -16,5 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* app.use('/', indexRouter); */
 /* app.use('/users', usersRouter); */
+
+
 
 module.exports = app;
